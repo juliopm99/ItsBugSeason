f21dae41270821f103c4467e32a3ee8fa9536729
ArtemAnk1 <33345147+ArtemAnk1@users.noreply.github.com>
2021-11-29 17:44:44 +0100

    cosas

---
+++ IdleBug/Assets/Arte/SeasonPantallaInicio.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeasonPantallaInicio : MonoBehaviour
+{
+    public float tiempoTotal;
+    float tiempoActual;
+    bool adelante =  true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        adelante = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tiempoActual += Time.realtimeSinceStartup;
+        if (adelante)
+        {
+            GetComponent<SeasonVisuales>().seasonValue = Mathf.Lerp(0, 1, tiempoActual / tiempoTotal);
+        }
+        else
+        {
+            GetComponent<SeasonVisuales>().seasonValue = Mathf.Lerp(1, 0, tiempoActual / tiempoTotal);
+        }
+
+        if(tiempoActual >= tiempoTotal)
+        {
+            tiempoActual = 0;
+            if (adelante)
+            {
+
+                adelante = false;
+            }
+            else
+            {
+                adelante = true;
+            }
+        }
+    }
+}
